c2c38e01703295ab48eb45390c1628e812476dce
hr955 <nfzoze01@gmail.com>
2024-07-04 20:33:22 +0900

    solve #2

--- src/bj_11720/Main.kt
+++ src/bj_11720/Main.kt
@@ -1,5 +1,24 @@
 package bj_11720
 
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
 fun main() {
-    print("??")
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val num = br.readLine()
+    val input = br.readLine()
+    var result = 0;
+
+    for(i: Int in 0..<num.toInt()){
+        result += input.substring(i,i+1).toInt()
+    }
+
+    bw.write("$result")
+
+    br.close()
+    bw.close()
 }
