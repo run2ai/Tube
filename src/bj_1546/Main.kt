0b23da685abd64e4aa1d3dc8f693c0edddc81e05
hr955 <nfzoze01@gmail.com>
2024-07-04 21:10:48 +0900

    solve #3

--- src/bj_1546/Main.kt
+++ src/bj_1546/Main.kt
@@ -1,20 +1,20 @@
 package bj_1546
 
+import java.util.StringTokenizer
+
 fun main() = with(System.`in`.bufferedReader()) {
-    val num = readLine()
-    val inputs = readLine().split(' ').toMutableList()
+    val num = readLine().toInt()
+    val inputs = StringTokenizer(readLine())
     var maxScore: Int = 0
     var scoreSum: Double = 0.0
 
-    for (i in inputs) {
-        if (maxScore < i.toInt()) {
-            maxScore = i.toInt()
+    for (i in 0..<num) {
+        val score = inputs.nextToken().toInt()
+        if(maxScore < score) {
+            maxScore = score
         }
+        scoreSum += score
     }
 
-    inputs.forEach { s ->
-        scoreSum += s.toDouble() / maxScore * 100
-    }
-
-    print(scoreSum / num.toInt())
+    print((scoreSum / maxScore * 100) / num)
 }
