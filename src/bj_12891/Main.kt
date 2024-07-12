fe7de5dba870c2b0505e2eac7f5e55239dbc5393
hr955 <nfzoze01@gmail.com>
2024-07-13 00:38:33 +0900

    fail #10 런타임 에러 (StringIndexOutOfBounds)

---
+++ src/bj_12891/Main.kt
@@ -0,0 +1,39 @@
+package bj_12891
+
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val input = StringTokenizer(readLine())
+    val (S, P) = input.nextToken().toInt() to input.nextToken().toInt()
+    val str = readLine()
+    val arrInput = StringTokenizer(readLine())
+    val rule = mapOf<String, Int>(
+        "A" to arrInput.nextToken().toInt(),
+        "C" to arrInput.nextToken().toInt(),
+        "G" to arrInput.nextToken().toInt(),
+        "T" to arrInput.nextToken().toInt()
+    )
+    var result = 0
+
+    repeat(S - P + 1) { i ->
+        var a = 0
+        var c = 0
+        var g = 0
+        var t = 0
+
+        val checkString = str.substring(i, i + S - 2)
+        for (element in checkString) {
+            when (element) {
+                'A' -> a++
+                'C' -> c++
+                'G' -> g++
+                'T' -> t++
+            }
+        }
+
+        if (a >= rule.getValue("A") && c >= rule.getValue("C") && g >= rule.getValue("G") && t >= rule.getValue("T")) {
+            result++
+        }
+    }
+    print(result)
+}
