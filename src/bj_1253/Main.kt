aaa87791b3cdafa5c64bd6d7e85a1d16690e070c
hr955 <nfzoze01@gmail.com>
2024-07-14 23:09:02 +0900

    solve #9

---
+++ src/bj_1253/Main.kt
@@ -0,0 +1,36 @@
+package bj_1253
+
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val N = readLine().toInt()
+    val input = StringTokenizer(readLine())
+    val arr = IntArray(N) { input.nextToken().toInt() }
+    var good = 0
+    arr.sort()
+
+    for (i in 0..<N) {
+        val target = arr[i]
+        var (start, end) = 0 to N - 1
+        while (start < end) {
+            val sum = arr[start] + arr[end]
+
+            if (sum == target) {
+                if (start != i && end != i) {
+                    good++
+                    break
+                } else if (start == i) {
+                    start++
+                } else {
+                    end--
+                }
+            } else if (sum > target) {
+                end--
+            } else {
+                start++
+            }
+        }
+    }
+
+    print(good)
+}
