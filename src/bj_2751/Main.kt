caeb6e498e189e803bcca986ed74dbd76cc893b9
hr955 <nfzoze01@gmail.com>
2024-07-28 15:26:45 +0900

    solve #21

--- src/bj_2751/Main.kt
+++ src/bj_2751/Main.kt
@@ -1,2 +1,19 @@
 package bj_2751
 
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val sb = StringBuilder()
+    val N = readLine().toInt()
+    val list = ArrayList<Int>()
+
+    repeat(N) {
+        list.add(readLine().toInt())
+    }
+
+    list.sort()
+    list.map { i ->
+        sb.appendLine(i)
+    }
+
+    print(sb)
+}
