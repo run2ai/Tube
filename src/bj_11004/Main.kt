f7bc202c8e9027c7bb163bb613855e1336ed3e0c
hr955 <nfzoze01@gmail.com>
2024-07-28 15:37:11 +0900

    solve #20

---
+++ src/bj_11004/Main.kt
@@ -0,0 +1,19 @@
+package bj_11004
+
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val st = StringTokenizer(readLine())
+    val N = st.nextToken().toInt()
+    val K = st.nextToken().toInt()
+    val arr = ArrayList<Int>()
+    val st2 = StringTokenizer(readLine())
+
+    repeat(N) {
+        arr.add(st2.nextToken().toInt())
+    }
+
+    arr.sort()
+
+    print(arr[K - 1])
+}
